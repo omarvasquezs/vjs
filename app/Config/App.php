33fc0c2bee8c954124be9008dd94f9c221afca57
Omar Vásquez <omar.vasquez@planleft.com>
2023-11-14 11:02:13 -0500

    Defaulting base_url and database

--- app/Config/App.php
+++ app/Config/App.php
@@ -16,7 +16,7 @@ class App extends BaseConfig
      *
      *    http://example.com/
      */
-    public string $baseURL = 'https://ci4test.ddev.site/';
+    public string $baseURL = 'https://staging.vjslaundry.com//';
 
     /**
      * Allowed Hostnames in the Site URL other than the hostname in the baseURL.
--- app/Config/Database.php
+++ app/Config/Database.php
@@ -26,10 +26,10 @@ class Database extends Config
      */
     public array $default = [
         'DSN'          => '',
-        'hostname'     => 'db',
-        'username'     => 'db',
-        'password'     => 'db',
-        'database'     => 'db',
+        'hostname'     => 'localhost',
+        'username'     => 'staging',
+        'password'     => 'D@mm1t1991',
+        'database'     => 'staging',
         'DBDriver'     => 'MySQLi',
         'DBPrefix'     => '',
         'pConnect'     => false,
